39260bc7885a762797b62d4f69c60aa2c65e7775
Olumoroti <olumoroti.anibaba@csedge.org>
2018-07-09 15:43:50 -0400

    Did Lesson 1, hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
 // Author: FirstName LastName
+console.log("Hello World!");
+console.log("It's a beautiful day for going out, isn't it?");
+console.log("Hey! What's 1 + 1?");
+console.log("It's " + 2 + 1 + " of course!");
+console.log("No, it's " + (100 + 1) + ", silly duck!");
+// console.log("This isn't going to get printed, is it?");
