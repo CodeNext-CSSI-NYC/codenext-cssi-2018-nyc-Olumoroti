9bacd2d4c1d2ec02c0671d92d8c7da3c63266718
Olumoroti <olumoroti.anibaba@csedge.org>
2018-07-10 17:00:35 -0400

    tweaked Pythagorean again

--- pythagorean-challenge.js
+++ pythagorean-challenge.js
@@ -6,7 +6,7 @@ console.log("%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%%^%^%^%^%^%^%^%^
 console.log("Welcome to Mo's Pythagorean Triplet Checker 2018!");
 var a = parseFloat(readline.question("Enter in the length of one leg: "));
 var b = parseFloat(readline.question("Enter in the length of the other leg: "));
-var c = parseFloat(readline.question("Enter in the length of the hypotenuse: "));
+var c = parseFloat(readline.question("Enter in the length of the hypotenuse (longest side): "));
 
 if((a ** 2) + (b ** 2) === (c ** 2)){
   console.log("The numbers " + a + ", " + b + ", and " + c + " are a Pythagorean Triplet. The triangle forms a right angle.");
@@ -18,6 +18,14 @@ else if((a ** 2) + (b ** 2) > (c ** 2)){
 
 else if((a ** 2) + (b ** 2) < (c ** 2)){
   console.log("The numbers " + a + ", " + b + ", and " + c + " are NOT a Pythagorean Triplet. The triangle forms an obtuse angle.");
+}
+
+if(a > c || b > c){
+  console.log("Oh, and a leg is NOT bigger than the hypotenuse, as the hypotenuse is the longest side of a triangle. Check your answer correctly again.");
+}
+
+if(a + b <= c || b + c <= a || a + c <= b){
+  console.log("Actually, this triangle wouldn't even be possible! LUL");
 }
 
 console.log("Thanks for using my program. Credits for original program go to myself with assitance from others. ™Google.");
