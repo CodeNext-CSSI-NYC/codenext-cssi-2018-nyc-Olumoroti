f0b3e35e27dcf7c849c9757a76213d901edb3cf7
Olumoroti <olumoroti.anibaba@csedge.org>
2018-07-10 16:51:00 -0400

    made small tweaks to pythagorean-challenge.js

--- pythagorean-challenge.js
+++ pythagorean-challenge.js
@@ -9,15 +9,15 @@ var b = parseFloat(readline.question("Enter in the length of the other leg: "));
 var c = parseFloat(readline.question("Enter in the length of the hypotenuse: "));
 
 if((a ** 2) + (b ** 2) === (c ** 2)){
-  console.log("The numbers" + a + ", " + b + ", and " + c + " are a Pythagorean Triplet. The triangle forms a right angle.");
+  console.log("The numbers " + a + ", " + b + ", and " + c + " are a Pythagorean Triplet. The triangle forms a right angle.");
 }
 
 else if((a ** 2) + (b ** 2) > (c ** 2)){
-  console.log("The numbers" + a + ", " + b + ", and" + c + " are NOT a Pythagorean Triplet. The triangle forms an acute angle.");
+  console.log("The numbers " + a + ", " + b + ", and " + c + " are NOT a Pythagorean Triplet. The triangle forms an acute angle.");
 }
 
 else if((a ** 2) + (b ** 2) < (c ** 2)){
-  console.log("The numbers" + a + ", " + b + ", and" + c + " are NOT a Pythagorean Triplet. The triangle forms an obtuse angle.");
+  console.log("The numbers " + a + ", " + b + ", and " + c + " are NOT a Pythagorean Triplet. The triangle forms an obtuse angle.");
 }
 
 console.log("Thanks for using my program. Credits for original program go to myself with assitance from others. ™Google.");
