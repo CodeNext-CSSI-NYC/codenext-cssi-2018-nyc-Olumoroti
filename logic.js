8e58d4f5c20d00456f653ea45da1d16c7a4b18b5
Olumoroti <olumoroti.anibaba@csedge.org>
2018-07-11 10:35:14 -0400

    finished logic.js

---
+++ logic.js
@@ -0,0 +1,48 @@
+// Author: Olumoroti Anibaba
+
+// Predict what each line will be (true or false) by writing down your answer in the comment
+// Then uncomment the middle lines one at a time to find out if you were right
+
+// 1.
+ true || false
+ console.log("1. My guess is: true");
+
+// 2.
+ !true || !false
+ console.log("2. My guess is: true");
+
+// 3.
+ false && !false
+ console.log("3. My guess is: false");
+
+// 4.
+ (true || false) && (true)
+ console.log("4. My guess is: true");
+
+// 5.
+ true || (false && true)
+ console.log("5. My guess is: true");
+
+// 6.
+ (false || !false) && (true || !true)
+ console.log("6. My guess is: true");
+
+// 7.
+ !(false || !false || !true)
+ console.log("7. My guess is: false");
+
+// 8.
+ 3 == "3" && 3 === "3"
+ console.log("8. My guess is: false");
+
+// 9.
+ 12 / 5 == 2
+ console.log("9. My guess is: false");
+
+// 10.
+ let a = 1;
+ 2 == a++;
+ console.log("10. My guess is: false");
+
+
+//copy this to repo
