6e6c5349e42f178ddb65efb259ebdd6ca642f9fa
Olumoroti <olumoroti.anibaba@csedge.org>
2018-07-12 16:14:59 -0400

    tweaked strings-practice.js to be efficient

--- strings-practice.js
+++ strings-practice.js
@@ -45,8 +45,8 @@ let a = lastTwo("a");
 
 function lastTwo(str) {
   if (str.length >= 2) {
-    return str.substring(0, (str.length - 2)) + str.substring(str.length - 1, str.length) +  str.substring(str.length - 2, str.length - 1);
-  } else {
+    return str.substring(0, (str.length - 2)) + str.charAt(str.length - 1) +  str.charAt(str.length - 2)
+    } else {
     return str;
   }
 
